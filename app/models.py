929ccc1265927e5b94957abb00ca38d41fb25c11
nrweir <nicholas.r.weir@gmail.com>
2018-02-09 17:54:45 -0500

    adding Oligos model, adding shell_context

--- app/models.py
+++ app/models.py
@@ -1,10 +1,26 @@
 from app import db
+from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    oligos = db.relationship('Oligos', backref='Creator', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Oligos(db.Model):
+    oligo_tube = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    oligo_name = db.Column(db.String(150), index=True)
+    date_added = db.Column(db.Date(), index=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    sequence = db.Column(db.String(2000))
+    restrixn_site = db.Column(db.String(20))
+    notes = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Oligo {}>'.format(self.sequence)
