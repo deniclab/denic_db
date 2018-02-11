8938f4e1fd4ca0bc731b93234bf727dd34b41430
nrweir <nicholas.r.weir@gmail.com>
2018-02-11 13:34:46 -0500

    beginning of 20180211 updates

--- app/views.py
+++ app/views.py
@@ -1,29 +1,73 @@
 """Views (URL:page refs) for the Denic intranet."""
 
-from app import app
-from flask import session, redirect, url_for, flash, render_template
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from flask import session, redirect, url_for, flash, render_template, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
-def home():
-    user = {'username': 'Nick'}  # mock user
-        # TODO: FINISH IMPLEMENTING THIS FUNCTION.
-
+@login_required
+def index():
+    return render_template('index.html', title='Home', posts='')  # TODO:fix
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if user tries to go to this page when they're already logged in
+    if current_user.is_authenticated:  # attr from flask_login UserMixin
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('home'))
+    if form.validate_on_submit():  # activated when submitted
+        # use SQLAlchemy query to get record for the user trying to login
+        user = User.query.filter_by(username=form.username.data).first()
+        # next line checks if user wasn't in db or if the password didn't match
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        # next, handle redirect to original page if sent by @login_required
+        next_page = request.args.get('next')
+        # if the user went straight to login (there wasn't a redirect to login)
+        # OR! if there was a full URL in the next argument (for security to
+        # prevent malicious redirects)
+        if not next_page or url_parse(next_page).netloc != '':
+            # set it up to redirect to index
+            next_page = url_for('index')
+        return redirect(next_page)
+    # render the login page if the form wasn't submitted already
     return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You have been successfully logged out.')
-    return redirect(url_for('home'))
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have successfully registered. The administrators have been contacted to approve.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [  # TRM
+        {'author': user, 'body': 'Test post #1'},  # TRM
+        {'author': user, 'body': 'Test post #2'}   # TRM
+    ]  # TRM
+    return render_template('user.html', user=user, posts=posts)
